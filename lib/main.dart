892b0dee43d070a52097d976627c56006f965874
shahfarazz <shahfarazulhaq@gmail.com>
2023-06-05 15:48:46 +0500

    merge

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'backend/services/authentication_service.dart';
 import 'frontend/Screens/Tenant/tenant_dashboard.dart';
-import 'frontend/Screens/splash.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
