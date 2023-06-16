f96d343eadfe83c49e2789fd3125d28caa4fdafe
Fawaz <23100025@lums.edu.pk>
2023-06-16 21:48:12 +0500

    FAQs Privacy Policy

--- lib/frontend/Screens/faq.dart
+++ lib/frontend/Screens/faq.dart
@@ -93,49 +93,76 @@ class FAQPage extends StatelessWidget {
                             ),
                             SizedBox(height: 16),
                             FAQCard(
-                              question: 'What is Flutter?',
+                              question: 'What is Rehnaa?',
                               answer:
-                                  'Flutter is a UI toolkit for building fast, natively compiled applications for mobile, web, and desktop from a single codebase.',
+                              'Rehnaa is Pakistan’s first digital rental platform that connects property owners and tenants in Pakistan. Our platform allows property owners to list their properties for rent and tenants to search and find suitable rental accommodations. Additionally, Rehnaa provides a seamless experience for its users through features such as KYC, guaranteed rent, loyalty points, advance rent, rent accrual, vouchers, legal support and much more.  Our aim is to instill trust and transparency in the rental market of Pakistan.'
                             ),
                             SizedBox(height: 16),
                             FAQCard(
-                              question: 'How can I get started with Flutter?',
+                              question: 'Does Rehnaa share our data further?',
                               answer:
-                                  'You can get started with Flutter by installing Flutter SDK, setting up your development environment, and creating a new Flutter project.',
-                            ),
-                            SizedBox(height: 16),
-                            FAQCard(
-                              question:
-                                  'Can I use Flutter for web development?',
-                              answer:
-                                  'Yes, you can use Flutter for web development. Flutter provides support for building web applications in addition to mobile and desktop platforms.',
+                              'Rehnaa does not share its users data with anyone except the users itself for better KYC profiling and user experience.'
                             ),
                             SizedBox(height: 16),
                             FAQCard(
                               question:
-                                  'Can I use Flutter for web development?',
+                                  'Is Rehnaa available in all cities of Pakistan?',
                               answer:
-                                  'Yes, you can use Flutter for web development. Flutter provides support for building web applications in addition to mobile and desktop platforms.',
+                              'Rehnaa is continually expanding its reach, but currently, our services are available in Lahore only. We aim to expand to more cities in the future, so stay tuned for updates.'
                             ),
                             SizedBox(height: 16),
                             FAQCard(
                               question:
-                                  'Can I use Flutter for web development?',
+                                  'How much does it cost to use Rehnaa?',
                               answer:
-                                  'Yes, you can use Flutter for web development. Flutter provides support for building web applications in addition to mobile and desktop platforms.',
+                                  'Registering an account and searching for properties on Rehnaa is free of charge for tenants. For property owners, there is subscription charges of 1% of monthly rent if they opt to use Rehnaa services till the end of the contract. For tenants, an upfront 15 days’ worth of rent is charged when a deal is brokered by Rehnaa. No other hidden charges exist. The specific fees and pricing details can be found by contacting our support team.'                            ),
+                            SizedBox(height: 16),
+                            FAQCard(
+                              question:
+                                  'How can I contact the property owner or tenant?',
+                              answer:
+                              'Once you find a property or tenant of interest on Rehnaa, you can click the request button on the listing so our support team can contact you directly through the platform to initiate further communication.'
                             ),
                             SizedBox(height: 16),
                             FAQCard(
                               question:
-                                  'Can I use Flutter for web development?',
+                                  'Can I negotiate the rental terms with the property owner?',
                               answer:
-                                  'Yes, you can use Flutter for web development. Flutter provides support for building web applications in addition to mobile and desktop platforms.',
+                              'Yes, Rehnaa encourages communication between tenants and property owners. You can negotiate rental terms, such as rent amount, duration, and any additional requirements directly with the property owner through our platforms messaging system'
+                            ),
+                            SizedBox(height: 16),
+                            FAQCard(
+                              question:
+                                  'Is my personal information safe on Rehnaa?',
+                              answer:
+                              'At Rehnaa, we take privacy and data security seriously. We have implemented robust security measures to protect your personal information. Please refer to our Privacy Policy for detailed information on how we collect, use, and safeguard your data.'
+                            ),
+                            SizedBox(height: 16),
+                            FAQCard(
+                              question:
+                                  'How can I report a problem or issue with a property or user?',
+                              answer:
+                              'If you encounter any problems or have concerns about a property or user on Rehnaa, please contact our support team immediately. We will investigate the issue and take appropriate action to resolve it.'
+                            ),
+                            SizedBox(height: 16),
+                            FAQCard(
+                              question:
+                                  'Can I list commercial properties for rent on Rehnaa?',
+                              answer:
+                              'Yes, Rehnaa deals in commercial properties as well.'
                             ),
                             SizedBox(height: 30),
-
+                            
                             // Add more FAQs
-                          ],
+                            
+                               
+                              ],
+
+                            
+
+                          
                         ),
+
                       ),
                     ),
                   ),
@@ -162,7 +189,6 @@ class FAQCard extends StatefulWidget {
   @override
   _FAQCardState createState() => _FAQCardState();
 }
-
 class _FAQCardState extends State<FAQCard> {
   bool _isExpanded = false;
 
@@ -194,8 +220,7 @@ class _FAQCardState extends State<FAQCard> {
                         decoration: TextDecoration.underline,
                         // fontWeight: FontWeight.bold,
                       ),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
+                      overflow: TextOverflow.visible,
                     ),
                   ),
                   IconButton(
@@ -214,7 +239,10 @@ class _FAQCardState extends State<FAQCard> {
               if (_isExpanded)
                 Padding(
                   padding: const EdgeInsets.only(top: 8.0),
-                  child: Text(widget.answer),
+                  child: Text(
+                    widget.answer,
+                    overflow: TextOverflow.visible,
+                  ),
                 ),
             ],
           ),
--- lib/frontend/Screens/privacypolicy.dart
+++ lib/frontend/Screens/privacypolicy.dart
@@ -110,41 +110,150 @@ class PrivacyPolicyPage extends StatelessWidget {
     );
   }
 
-  Widget buildTextCard() {
-    const String privacyPolicyText =
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
-        "Vestibulum ac cursus velit. Sed rhoncus consequat tortor, "
-        "quis fringilla nisi consectetur a. Sed commodo mauris in "
-        "erat varius commodo. Nam ultrices facilisis nunc a interdum. "
-        "Fusce interdum placerat orci, in fringilla arcu fringilla id. "
-        "Suspendisse potenti. Aenean lobortis sagittis lorem at ultrices. "
-        "Fusce lacinia felis sit amet nunc luctus, ac hendrerit metus "
-        "luctus. Suspendisse rhoncus, mi nec feugiat convallis, mauris "
-        "tellus varius purus, ac mattis massa ligula id metus. Nullam "
-        "ut ligula eu sem interdum auctor nec sed est. Suspendisse "
-        "eget interdum odio. Nullam rhoncus volutpat tellus, id sagittis "
-        "augue dictum id. Nam bibendum id ex et tincidunt. Donec maximus "
-        "dictum risus, vitae posuere enim sollicitudin sed. Nullam "
-        "tristique, dui vel viverra eleifend, lorem nisl sagittis odio, "
-        "quis facilisis arcu odio id ex.";
-
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20.0),
-      ),
-      child: const Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Text(
-          privacyPolicyText,
-          style: TextStyle(
-            fontSize: 16,
-            height: 1.8, // Increase line spacing (adjust the value as needed)
-            fontFamily: 'Roboto', // Set your desired font family
-            color: Colors.black87,
-          ),
-          textAlign: TextAlign.justify, // Justify the text
+Widget buildTextCard() {
+  return Card(
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(20.0),
+    ),
+    child: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: SelectableText.rich(
+        TextSpan(
+          children: <TextSpan>[
+            TextSpan(
+              text: 'Privacy Policy Rehnaa.pk\n\n',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+            TextSpan(
+              text:
+                  'At Rehnaa, we are committed to protecting the privacy and personal information of our users. This Privacy Policy outlines how we collect, use, disclose, and safeguard the information you provide when using our rental platform ("Platform"). By accessing or using the Platform, you agree to the terms of this Privacy Policy.\n\n',
+            ),
+            TextSpan(
+              text: 'Information Collection\n',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextSpan(
+              text:
+                  '1.1 Personal Information: When you create an account on Rehnaa, we may collect personal information, including but not limited to your name, email address, phone number, and location.\n\n',
+            ),
+            TextSpan(
+              text:
+                  '1.2 Property Listings: To facilitate the rental process, we collect information about the properties you list on the Platform, including property details, rental rates, availability, and any additional information you choose to provide.\n\n',
+            ),
+            TextSpan(
+              text:
+                  '1.3 Payment Information: If you choose to make or receive payments through our Platform, we may collect payment information, such as credit card details or bank account information, to process these transactions securely. Please note that we do not store complete payment information on our servers.\n\n',
+            ),
+            TextSpan(
+              text:
+                  '1.4 Usage Information: We may collect information about your interactions with the Platform, such as your IP address, device information, browser type, and usage patterns. This information helps us analyze trends, administer the Platform, and improve user experience.\n\n',
+            ),
+            TextSpan(
+              text: 'Information Use\n',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextSpan(
+              text:
+                  '2.1 Providing Services: We use the information we collect to provide and improve our services, facilitate property listings and rental transactions, verify user identity, respond to inquiries and requests, and personalize your experience on the Platform.\n\n',
+            ),
+            TextSpan(
+              text:
+                  '2.2 Communication: We may use your contact information to send you important notices, updates, promotional materials, and other communications related to the Platform. You can opt-out of receiving these communications at any time.\n\n',
+            ),
+            TextSpan(
+              text:
+                  '2.3 Aggregated Data: We may aggregate and anonymize the collected information to generate statistical or analytical insights that help us understand trends, preferences, and user behavior. This aggregated data does not identify any individual and may be used for research or marketing purposes.\n\n',
+            ),
+            TextSpan(
+              text: 'Information Sharing\n',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextSpan(
+              text:
+                  '3.1 Service Providers: We may engage trusted third-party service providers to perform functions on our behalf, such as hosting, data analysis, payment processing, customer support, and marketing activities. These providers have access to personal information solely for the purpose of performing their duties and are obligated to maintain its confidentiality.\n\n',
+            ),
+            TextSpan(
+              text:
+                  '3.2 Legal Compliance: We may disclose your personal information if required to do so by law or in response to valid legal requests, such as subpoenas, court orders, or government regulations. We may also disclose information when we believe in good faith that it is necessary to protect our rights, enforce our terms and policies, or investigate and prevent potential fraud or security breaches.\n\n',
+            ),
+            TextSpan(
+              text:
+                  '3.3 Business Transfers: In the event of a merger, acquisition, or sale of our business assets, your personal information may be transferred as part of the transaction. We will notify you via email or a prominent notice on the Platform if such a change in ownership occurs, as well as any choices you may have regarding your information.\n\n',
+            ),
+            TextSpan(
+              text: 'Data Security\n',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextSpan(
+              text:
+                  'We employ industry-standard security measures to protect your personal information from unauthorized access, disclosure, alteration, or destruction. However, please note that no method of transmission over the Internet or electronic storage is entirely secure, and we cannot guarantee absolute security.\n\n',
+            ),
+            TextSpan(
+              text: 'User Rights and Choices\n',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextSpan(
+              text:
+                  '5.1 Account Management: You have the right to review, update, or delete your account information at any time. You may also edit or remove property listings or adjust your communication preferences. Please note that certain information may be retained as required by law or for legitimate business purposes.\n\n',
+            ),
+            TextSpan(
+              text:
+                  '5.2 Cookies and Tracking Technologies: We use cookies and similar tracking technologies to enhance your user experience, analyze usage patterns, and deliver personalized content. You can modify your browser settings to manage cookies or opt-out of certain tracking technologies, but this may limit the functionality of the Platform.\n\n',
+            ),
+            TextSpan(
+              text: 'Children\'s Privacy\n',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextSpan(
+              text:
+                  'Rehnaa is not intended for use by individuals under the age of 18. We do not knowingly collect personal information from children. If you believe we have inadvertently collected information from a child, please contact us to request deletion.\n\n',
+            ),
+            TextSpan(
+              text: 'Changes to the Privacy Policy\n',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextSpan(
+              text:
+                  'We reserve the right to modify or update this Privacy Policy at any time. We will notify you of significant changes by posting a prominent notice on the Platform or sending you an email. Your continued use of the Platform after such modifications indicate your acknowledgment and acceptance of the updated Privacy Policy.\n\n',
+            ),
+            TextSpan(
+              text: 'Contact Us\n',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextSpan(
+              text:
+                  'If you have any questions, concerns, or suggestions regarding this Privacy Policy or our privacy practices, please contact us at [insert contact information].\n\n',
+            ),
+            TextSpan(
+              text:
+                  'By using the Rehnaa Rental Platform, you consent to the collection, use, disclosure, and processing of your information as described in this Privacy Policy.',
+            ),
+          ],
         ),
+        textAlign: TextAlign.justify, // Justify the text
       ),
-    );
-  }
+    ),
+  );
+}
+
+
 }
